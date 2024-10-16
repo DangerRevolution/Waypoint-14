706c3970b6fc4d74a28fcfde159597c8ec2c88ff
DangerRevolution <142105406+DangerRevolution@users.noreply.github.com>
2024-10-16 14:04:35 +0100

    update MapInitTest

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -49,21 +49,21 @@ namespace Content.IntegrationTests.Tests
             "CentComm",
             "MeteorArena",
             "NukieOutpost",
-            "Core",
-            "Pebble", //DeltaV
-            "Edge", //DeltaV
-            "Saltern",
-            "Shoukou", //DeltaV
-            "Tortuga", //DeltaV
-            "Arena", //DeltaV
-            "Asterisk", //DeltaV
-            "Glacier", //DeltaV
-            "TheHive", //DeltaV
-            "Hammurabi", //DeltaV
-            "Lighthouse", //DeltaV
-            "Submarine", //DeltaV
-            "Gax",
-            "Rad"
+            // "Core",
+            // "Pebble", //DeltaV
+            // "Edge", //DeltaV
+             "Saltern",
+            // "Shoukou", //DeltaV
+            // "Tortuga", //DeltaV
+            // "Arena", //DeltaV
+            // "Asterisk", //DeltaV
+            // "Glacier", //DeltaV
+            // "TheHive", //DeltaV
+            // "Hammurabi", //DeltaV
+            // "Lighthouse", //DeltaV
+            // "Submarine", //DeltaV
+            // "Gax",
+            // "Rad"
         };
 
         /// <summary>
